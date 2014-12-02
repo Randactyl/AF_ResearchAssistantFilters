af61dafc891b5d58881049e6167e3e972299f019
Randactyl <rafowler@randactyl.com>
2014-12-01 18:31:18 -0800

    Initial commit. Addon version 1.1.0.1

---
+++ AF_ResearchAssistantFilters.lua
@@ -0,0 +1,63 @@
+local function GetFilterCallbackForResearch(researchType)
+	return function ( slot )
+		local data
+		if slot.dataEntry then
+			data = slot.dataEntry.data.researchAssistant
+		else
+			return false
+		end
+		if(data == researchType) then
+			return true
+		end
+		return false
+	end
+end
+
+local ResearchAssistantDropdownCallbacks = {
+	[1] = { name = "Researchable", filterCallback = GetFilterCallbackForResearch("researchable")},
+	[2] = { name = "Duplicate", filterCallback = GetFilterCallbackForResearch("duplicate")},
+	[3] = { name = "Known", filterCallback = GetFilterCallbackForResearch("known")}
+}
+
+local en = {
+	["Researchable"] = "Researchable",
+	["Duplicate"]	 = "Duplicate",
+	["Known"] 		 = "Known"
+}
+local de = {
+	["Researchable"] = "Analysierbar",
+	["Duplicate"]	 = "Doppelt",
+	["Known"] 		 = "Bekannt"
+}
+local fr = {
+	["Researchable"] = "Recherchable",
+	["Duplicate"]	 = "Double",
+	["Known"] 		 = "Connu"
+}
+
+local filterInformation = {
+	callbackTable = ResearchAssistantDropdownCallbacks,
+	filterType = ITEMFILTERTYPE_WEAPONS,
+	subfilters = {
+		[1] = "All",
+	},
+	enStrings = en,
+	deStrings = de,
+	frStrings = fr,
+}
+
+AdvancedFilters_RegisterFilter(filterInformation)
+
+filterInformation = {
+	callbackTable = ResearchAssistantDropdownCallbacks,
+	filterType = ITEMFILTERTYPE_ARMOR,
+	subfilters = {
+		[1] = "Body",
+		[2] = "Shield",
+	},
+	enStrings = en,
+	deStrings = de,
+	frStrings = fr,
+}
+
+AdvancedFilters_RegisterFilter(filterInformation)
